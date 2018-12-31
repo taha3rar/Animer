a87b0086613060d94a5931cd2bc9bf12d9cd2c1d
Javi Esses <javi.esses@gmail.com>
2018-12-31 09:41:19 -0300

    Invoice list shared component

---
+++ src/app/shared/list-components/invoice-list/invoice-list.component.ts
@@ -0,0 +1,25 @@
+import { Component, OnInit, Input } from '@angular/core';
+import { Invoice } from '@app/core/models/order/invoice';
+
+@Component({
+  selector: 'app-invoice-list',
+  templateUrl: './invoice-list.component.html',
+  styleUrls: ['./invoice-list.component.scss']
+})
+export class InvoiceListComponent implements OnInit {
+  @Input()
+  invoices: Invoice[];
+  page = 1;
+
+  constructor() {}
+
+  ngOnInit() {}
+
+  warning() {
+    swal({
+      title: 'Are you sure?',
+      text: 'Once deleted, you will not be able to recover this invoice!',
+      icon: 'warning'
+    });
+  }
+}
