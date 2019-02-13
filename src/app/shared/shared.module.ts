22599f029f32d461e48d6971cfa13ead41ea38d4
Rihamkh <rihamkh.114@gmail.com>
2019-02-13 13:46:54 +0200

    uneseccary import

--- src/app/shared/shared.module.ts
+++ src/app/shared/shared.module.ts
@@ -18,7 +18,6 @@ import { BaseNavigationComponent } from './components/base-navigation/base-navig
 import { UploadPictureComponent } from './components/upload-picture/upload-picture.component';
 import { BaseValidationComponent } from './components/base-validation/base-validation.component';
 import { UploadPictureComponent } from './components/upload-picture/upload-picture.component';
-import { BaseValidationComponent } from './components/base-validation/base-validation.component';
 
 @NgModule({
   imports: [CommonModule, NgxPaginationModule, NgbModule, RouterModule],
