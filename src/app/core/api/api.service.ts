b6811ae189d4aacc47a23fbf289d51a015d69ece
Javi Esses <javi.esses@gmail.com>
2019-02-21 16:35:34 -0300

    Removed error catching from APi calls so get the proper erros

--- src/app/core/api/api.service.ts
+++ src/app/core/api/api.service.ts
@@ -11,43 +11,36 @@ export class ApiService {
   constructor(private httpClient: HttpClient) {}
 
   get(path: string, params: HttpParams = new HttpParams()): Observable<any> {
-    return this.httpClient
-      .cache()
-      .get(`${environment.api_url}${path}`, { params: params })
-      .pipe(catchError((err: any) => of(err)));
+    return this.httpClient.cache().get(`${environment.api_url}${path}`, { params: params });
+    // .pipe(catchError((err: any) => of(err)));
   }
 
   getAll(path: string): Observable<any> {
-    return this.httpClient
-      .cache()
-      .get(`${environment.api_url}${path}`)
-      .pipe(catchError((err: any) => of(err)));
+    return this.httpClient.cache().get(`${environment.api_url}${path}`);
+    // .pipe(catchError((err: any) => of(err)));
   }
 
   put(path: string, body: Object = {}, params: HttpParams = new HttpParams()): Observable<any> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-    return this.httpClient
-      .put(`${environment.api_url}${path}`, JSON.stringify(body), {
-        headers: headers,
-        params: params
-      })
-      .pipe(catchError((err: any) => of(err)));
+    return this.httpClient.put(`${environment.api_url}${path}`, JSON.stringify(body), {
+      headers: headers,
+      params: params
+    });
+    // .pipe(catchError((err: any) => of(err)));
   }
 
   post(path: string, body: Object = {}): Observable<any> {
     const headers = new HttpHeaders({ 'Content-Type': 'application/json' });
 
-    return this.httpClient
-      .post(`${environment.api_url}${path}`, JSON.stringify(body), { headers: headers })
-      .pipe(catchError((err: any) => of(err)));
+    return this.httpClient.post(`${environment.api_url}${path}`, JSON.stringify(body), { headers: headers });
+    // .pipe(catchError((err: any) => of(err)));
   }
 
   delete(path: string, params: HttpParams = new HttpParams()): Observable<any> {
-    return this.httpClient
-      .delete(`${environment.api_url}${path}`, {
-        params: params
-      })
-      .pipe(catchError(catchError((err: any) => of(err))));
+    return this.httpClient.delete(`${environment.api_url}${path}`, {
+      params: params
+    });
+    // .pipe(catchError(catchError((err: any) => of(err))));
   }
 }
