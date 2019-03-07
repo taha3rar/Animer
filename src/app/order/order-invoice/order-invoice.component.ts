67924824f75286eeb056e8c3f47f7f6cd54d7f74
Javi Esses <javi.esses@gmail.com>
2019-03-07 10:44:35 -0300

    Removed unnecesary packages for build

--- src/app/order/order-invoice/order-invoice.component.ts
+++ src/app/order/order-invoice/order-invoice.component.ts
@@ -2,8 +2,6 @@ import { Component, OnInit, Input } from '@angular/core';
 import { Invoice } from '@app/core/models/invoice/invoice';
 import { Router } from '@angular/router';
 import { InvoiceService } from '@app/core/api/invoice.service';
-import * as jspdf from 'jspdf';
-import * as html2canvas from 'html2canvas';
 
 @Component({
   selector: 'app-order-invoice',
@@ -19,15 +17,6 @@ export class OrderInvoiceComponent implements OnInit {
   constructor(private invoiceService: InvoiceService, private router: Router) {}
 
   ngOnInit() {}
-
-  download(): void {
-    html2canvas(document.getElementById('pdfContent'), { windowWidth: 900, windowHeight: 1000 }).then(canvas => {
-      const pdf = new jspdf('p', 'mm', 'a4');
-      const img = canvas.toDataURL('image/png');
-      pdf.addImage(img, 'PNG', 0, 0, 208, 300);
-      pdf.save(`invoice-${this.invoice.numericId}.pdf`);
-    });
-  }
 
   saveInvoice(): void {
     this.invoiceService.create(this.invoice).subscribe(data => {
