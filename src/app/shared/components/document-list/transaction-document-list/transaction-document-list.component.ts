27d7ad9a8bd0aac49a86b256d2ee4a0569df98b3
Javi Esses <javi.esses@gmail.com>
2019-03-08 12:50:19 -0300

    File uploaders validations

--- src/app/shared/components/document-list/transaction-document-list/transaction-document-list.component.ts
+++ src/app/shared/components/document-list/transaction-document-list/transaction-document-list.component.ts
@@ -3,6 +3,7 @@ import { BaseListComponent } from '../../base-list/base-list.component';
 import { DocumentService } from '@app/core/api/document.service';
 import { Router } from '@angular/router';
 import { Document } from '@app/core/models/order/document';
+declare const $: any;
 
 @Component({
   selector: 'app-transaction-document-list',
@@ -38,19 +39,37 @@ export class TransactionDocumentListComponent extends BaseListComponent implemen
     const reader = new FileReader();
     if (event.target.files && event.target.files.length > 0) {
       const file = event.target.files[0];
-      reader.readAsDataURL(file);
-      reader.onload = () => {
-        const document = new Document();
-        document.file_name = file.name.replace(/\.[^/.]+$/, '');
-        document.transaction_id = this.transaction_id;
-        const base64File = (reader.result as string).split(',')[1];
-        document.file = base64File;
-        this.documentService.create(document).subscribe(() => this.router.navigate([this.router.url]));
-      };
+      if (this.validateFile(file)) {
+        reader.readAsDataURL(file);
+        reader.onload = () => {
+          const document = new Document();
+          document.file_name = file.name.replace(/\.[^/.]+$/, '');
+          document.transaction_id = this.transaction_id;
+          const base64File = (reader.result as string).split(',')[1];
+          document.file = base64File;
+          this.documentService.create(document).subscribe(() => this.router.navigate([this.router.url]));
+        };
+      } else {
+        $.notify(
+          {
+            icon: 'notifications',
+            message: 'Maximum file size is 3MB and it should be .jpg, .pdf or .png'
+          },
+          {
+            type: 'danger',
+            timer: 5000,
+            placement: {
+              from: 'top',
+              align: 'right'
+            },
+            offset: 78
+          }
+        );
+      }
     }
   }
 
   validateFile(file: any) {
-    return this.acceptedMimeTypes.includes(file.type) && file.size < 5e6;
+    return this.acceptedMimeTypes.includes(file.type) && file.size < 3e6;
   }
 }
