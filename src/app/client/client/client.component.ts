7363c5a9ff3d3ffb0654a2931768cdd53f9f60ae
Michael Marcus <marcus.mika@gmail.com>
2019-05-05 13:51:18 +0300

    Client's guard now filters sellers and buyers

--- src/app/client/client/client.component.ts
+++ src/app/client/client/client.component.ts
@@ -3,14 +3,12 @@ import { Location } from '@angular/common';
 import { ActivatedRoute } from '@angular/router';
 import { User } from '@app/core/models/user/user';
 import { defaultValues } from '@app/shared/helpers/default_values';
-import { Transaction } from '@app/core/models/transaction';
 import { Invoice } from '@app/core/models/invoice/invoice';
 import { Order } from '@app/core/models/order/order';
 
 export class Counter {
   orders: number;
   documents: number;
-  // transactions: number;
   invoices: number;
 }
 
@@ -32,13 +30,11 @@ export class ClientComponent implements OnInit {
   ngOnInit() {
     this.route.data.subscribe(({ user, orders, invoices, documents }) => {
       this.user = user;
-      // this.transactions = transactions;
       this.orders = orders;
       this.invoices = invoices;
       this.documents = documents;
 
       this.counter = {
-        // transactions: this.transactions.length,
         orders: this.orders.length,
         documents: this.documents.length,
         invoices: this.invoices.length
