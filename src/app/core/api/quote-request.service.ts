ef3fd8c5d5ab55a4e44ee7fd85ceb45cdab939e4
Javi Esses <javi.esses@gmail.com>
2019-05-22 15:12:32 -0300

    Removed unnecesary code and used ngxPermissions

--- src/app/core/api/quote-request.service.ts
+++ src/app/core/api/quote-request.service.ts
@@ -1,9 +1,6 @@
 import { Injectable } from '@angular/core';
 import { BaseService } from './base.service';
 import { ApiService } from './api.service';
-import { QuoteRequest } from '../models/quote-request/quoteRequest';
-import { Observable } from 'rxjs';
-import { map } from 'rxjs/operators';
 
 @Injectable({
   providedIn: 'root'
@@ -11,25 +8,5 @@ import { map } from 'rxjs/operators';
 export class OrderService extends BaseService {
   constructor(protected apiService: ApiService) {
     super(apiService, '/quote-request');
-  }
-
-  getByUserIdAndClientId(userId: string, clientId: string): Observable<QuoteRequest[]> {
-    return this.apiService.get(`/user/${userId}/client/${clientId}/quote-request`).pipe(map(data => data));
-  }
-
-  getByUserAsBuyer(userId: string): Observable<QuoteRequest[]> {
-    return this.apiService.get(`${this.path}/user/${userId}/buyer`).pipe(map(data => data));
-  }
-
-  getByUserAsSeller(userId: string): Observable<QuoteRequest[]> {
-    return this.apiService.get(`${this.path}/user/${userId}/seller`).pipe(map(data => data));
-  }
-
-  getPdf(orderId: string, version: string): Observable<Blob> {
-    return this.apiService.getPdf(`${this.path}/${orderId}/pdf/${version}`).pipe(map(data => data));
-  }
-
-  draft(order: QuoteRequest) {
-    return this.apiService.post(`/quote-request/draft`, order).pipe(map(data => data));
   }
 }
