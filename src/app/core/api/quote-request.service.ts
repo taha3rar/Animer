da067d867d9df28bdcd17bd5d8874ac6cee5db56
Michael Marcus <marcus.mika@gmail.com>
2019-05-26 12:13:12 +0300

    QR model and service

--- src/app/core/api/quote-request.service.ts
+++ src/app/core/api/quote-request.service.ts
@@ -1,6 +1,8 @@
 import { Injectable } from '@angular/core';
 import { BaseService } from './base.service';
 import { ApiService } from './api.service';
+import { map } from 'rxjs/operators';
+import { QuoteRequest } from '../models/quote-request/quoteRequest';
 
 @Injectable({
   providedIn: 'root'
@@ -8,5 +10,9 @@ import { ApiService } from './api.service';
 export class OrderService extends BaseService {
   constructor(protected apiService: ApiService) {
     super(apiService, '/quote-request');
+  }
+
+  draft(quoteRequest: QuoteRequest) {
+    return this.apiService.post(`/quote-request/draft`, quoteRequest).pipe(map(data => data));
   }
 }
--- src/app/core/models/quote-request/quoteRequest.ts
+++ src/app/core/models/quote-request/quoteRequest.ts
@@ -1,1 +1,71 @@
-export class QuoteRequest {}
+export class QuoteRequest {
+  buyer: {
+    _id: String;
+    first_name: String;
+    last_name: String;
+    email: String;
+    phone_number: String;
+    contact_by: [String];
+    company_name: String;
+    address: String;
+    city: String;
+    zipcode: String;
+    country: String;
+  };
+  sellers: [
+    {
+      _id: String;
+      first_name: String;
+      last_name: String;
+      email: String;
+      phone_number: String;
+      contact_by: [String];
+      company_name: String;
+      address: String;
+      city: String;
+      zipcode: String;
+      country: String;
+    }
+  ];
+  currency: String;
+  comments: String;
+  valid_by: Date;
+  draft: Boolean;
+  products: [
+    {
+      product_type: String;
+      specification: String;
+      produce: String;
+      variety: String;
+      type_of_package: String;
+      package_weight: Number;
+      weight_unit: String;
+      total_weight: Number;
+      item_package_type: String;
+      item_measurement_amount: Number;
+      item_measurement_unit: String;
+      items_per_package: String;
+      total_amount_items: Number;
+      product_subtotal: Number;
+      quantity: Number;
+      out_of_inventory: Boolean;
+      has_package_details: Boolean;
+    }
+  ];
+  document_weight_unit: String;
+  total_packages: Number;
+  total_weight: Number;
+  quotations: [
+    {
+      _id: String;
+      total_price: Number;
+      user_id: String;
+      date_created: Date;
+      status: {
+        buyer: String;
+        seller: String;
+      };
+    }
+  ];
+  status: String;
+}
