a9cfbfd14d3703e7c30776574da884c411ebf5fd
Rihamkh <rihamkh.114@gmail.com>
2019-06-03 10:27:16 +0300

    Text fixes

--- src/app/quote-request/quotation-view/quotation-view.component.ts
+++ src/app/quote-request/quotation-view/quotation-view.component.ts
@@ -1,5 +1,6 @@
 import { Component, OnInit, Input } from '@angular/core';
 import { Location } from '@angular/common';
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-quotation-view',
@@ -16,5 +17,21 @@ export class QuotationViewComponent implements OnInit {
 
   back() {
     this.location.back();
+  }
+
+  downloadPopup() {
+    swal({
+      title: 'Download as PDF',
+      className: 'swal-pdf',
+      text: 'Please choose the type of quotation document you would like to download:',
+      buttons: {
+        originalDoc: { text: 'Original Document', value: 'original', className: 'swal-button-o', closeModal: false },
+        copyDoc: { text: 'Copy Document', value: 'copy', closeModal: false }
+      }
+    }).then(value => {
+      if (value === 'original') {
+      } else {
+      }
+    });
   }
 }
--- src/app/quote-request/quote-request.component.ts
+++ src/app/quote-request/quote-request.component.ts
@@ -1,4 +1,5 @@
 import { Component, OnInit } from '@angular/core';
+import swal from 'sweetalert';
 
 @Component({
   selector: 'app-quote-request',
@@ -8,4 +9,20 @@ export class QuoteRequestComponent implements OnInit {
   constructor() {}
 
   ngOnInit() {}
+
+  downloadPopup() {
+    swal({
+      title: 'Download as PDF',
+      className: 'swal-pdf',
+      text: 'Please choose the type of quote request document you would like to download:',
+      buttons: {
+        originalDoc: { text: 'Original Document', value: 'original', className: 'swal-button-o', closeModal: false },
+        copyDoc: { text: 'Copy Document', value: 'copy', closeModal: false }
+      }
+    }).then(value => {
+      if (value === 'original') {
+      } else {
+      }
+    });
+  }
 }
