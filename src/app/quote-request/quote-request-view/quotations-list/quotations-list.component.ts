fb307228482e5aa09ade114044fdc5c9cd7f8323
Michael Marcus <marcus.mika@gmail.com>
2019-06-11 10:42:34 +0300

    Quotation view implementation beginning

--- src/app/quote-request/quote-request-view/quotations-list/quotations-list.component.ts
+++ src/app/quote-request/quote-request-view/quotations-list/quotations-list.component.ts
@@ -3,6 +3,8 @@ import { QuoteRequest } from '@app/core/models/quote-request/quoteRequest';
 import { Quotation } from '@app/core/models/quotation/quotation';
 import { QuotationService } from '@app/core';
 import { ActivatedRoute } from '@angular/router';
+import { ProductQuotation } from '@app/core/models/quotation/product-quotation';
+import { MatDialog, MatDialogConfig } from '@angular/material';
 
 @Component({
   selector: 'app-quotations-list',
@@ -10,17 +12,39 @@ import { ActivatedRoute } from '@angular/router';
   styleUrls: ['./quotations-list.component.scss']
 })
 export class QuotationsListComponent implements OnInit {
+  @Input() acceptedQuotations = false;
   page = 1;
   quoteRequest: QuoteRequest;
   quotations: Quotation[];
+  viewProduct: ProductQuotation;
 
-  constructor(private quotationService: QuotationService, private route: ActivatedRoute) {}
+  constructor(private quotationService: QuotationService, private route: ActivatedRoute, public dialog: MatDialog) {}
 
   ngOnInit() {
     this.quoteRequest = this.route.snapshot.data['quoteRequest'];
     this.quotationService.getByQuoteRequest(this.quoteRequest._id).subscribe(quotations => {
-      this.quotations = quotations;
+      if (this.acceptedQuotations) {
+        this.quotations = quotations.filter(quotation => quotation.status === 'QUOTATION ACCEPTED');
+      } else {
+        this.quotations = quotations;
+      }
       console.log(this.quotations);
     });
   }
+
+  // openQuotationView(index: number): void {
+  //   const dialogConfig = new MatDialogConfig();
+
+  //   dialogConfig.autoFocus = true;
+  //   // dialogConfig.height = height;
+  //   // dialogConfig.width = '850px';
+  //   dialogConfig.data = {quotation : this.quotations[index]};
+
+  //   const dialogRef = this.dialog.open(component, dialogConfig);
+  //   // dialogRef.afterClosed().subscribe(data => {
+  //   //   if (data) {
+  //   //     this.updateProduct(data.product, data.index);
+  //   //   }
+  //   // });
+  // }
 }
