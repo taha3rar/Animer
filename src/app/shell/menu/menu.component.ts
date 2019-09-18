af7a508444d253c8be9038f9a9e06365fe487473
Rihamkh <rihamkh.114@gmail.com>
2019-09-18 13:58:49 +0300

    hamburger menu and intercom change

--- src/app/shell/menu/menu.component.ts
+++ src/app/shell/menu/menu.component.ts
@@ -3,6 +3,8 @@ import { Component, OnInit } from '@angular/core';
 import { AuthenticationService } from '@app/core';
 import { Credentials } from '@app/core/models/user/login-models';
 
+declare const $: any;
+
 export interface RouteInfo {
   path: string;
   title: string;
@@ -78,6 +80,11 @@ export class MenuComponent implements OnInit {
       this.menuItems = ROUTES.filter(mi =>
         mi.neededPermission ? this.currentCredentials.user.permissions.some(p => p === mi.neededPermission) : true
       );
+    }
+  }
+  hideSidebar() {
+    if ($('.navbar-collapse').is(':visible')) {
+      $('.navbar-collapse').collapse('hide');
     }
   }
 }
