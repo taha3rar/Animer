b7176560618bf0152c4d4687c4a6374d2b98c163
taha3rar <taha3rar@gmail.com>
2020-03-19 15:52:23 +0200

    cleaned up the code a bit

--- server.js
+++ server.js
@@ -1,18 +1,32 @@
-var express = require("express");
-var app = express();
-var cors = require('cors')
+const express = require("express");
+const app = express();
+const cors = require('cors')
 const puppeteer = require('puppeteer');
 const anime = require('gogoanime')
-anime.animeEpisodeHandler('naruto-episode-1').then(ep => {
-    console.log(ep[0].servers[0].iframe); // this could be a url for any episode just change 'naruto-episode-1' to any anime + '-episode-number' 
-})
-app.use(cors({ origin: 'http://localhost:8888' }));
+let numberOfResponses = 0;
+app.use(cors({ origin: 'http://localhost:8888' }));//idk
 app.get('/url', (req, res, next) => {
-    var url = req.url.substr(req.url.indexOf('?') + 1)// i send the link for the episode with ?{link} and this is how i get the link in express
-    console.log(url);//https://vidstreaming.io/streaming.php?id=MjUwNTQ=&title=Naruto+Episode+1 ins this case
+    let url = req.url.substr(req.url.indexOf('?') + 1)//probably not the best way to get the param but it works lol // i send the link for the episode with ?{link} and this is how i get the link in express
+    console.log(url);//https://vidstreaming.io/streaming.php?id=MjUwNTQ=&title=Naruto+Episode+1 in  this case
     if (url.includes('http')) {
         pup(url, res);
     }
+
+});
+app.get('/anime', (req, res, next) => {
+    let url = req.url.substr(req.url.indexOf('?') + 1)// i send the link for the episode with ?{link} and this is how i get the link in express
+    console.log(url);//anime name and episode in this case
+    if (url.includes('episode')) {
+        anime.animeEpisodeHandler(url).then(ep => {
+            let link = "https://" + ep[0].servers[0].iframe
+            console.log(ep[0].servers[0].iframe)
+            pup(link, res);
+        })
+    }
+
+});
+app.get('/uses', (req, res, next) => {
+    res.send(numberOfResponses);
 
 });
 app.listen(process.env.PORT || 3000, () => {
@@ -20,11 +34,10 @@ app.listen(process.env.PORT || 3000, () => {
 });
 
 async function pup(url, res) {
-    //executablePath: 'Google/Chrome/Application/chrome',
     const browser = await puppeteer.launch({
         args: [
             '--no-sandbox',
-            '--disable-setuid-sandbox',
+            '--disable-setuid-sandbox',// these two args for heroku to use puppeteer
         ],
     });
     const page = await browser.newPage();
@@ -32,19 +45,15 @@ async function pup(url, res) {
     await page.waitForSelector('video')
     await page.click('video');
     await page.waitFor(500);
-    let pages = await browser.pages();
-    // await pages[2].close();//first click opens an ad in a new tab and this closes it
-    console.log(pages.length);
     await page.click('video');
     await page.waitForFunction('document.querySelector("video").getAttribute("src").includes("http")');
-    console.log('Value changed!')
     let p = await page.evaluate('document.querySelector("video").getAttribute("src")'); //this is how i get the src and thats what i return
     await browser.close();
     res.setHeader('Access-Control-Allow-Origin', '*');// i dont know what this is 
     res.setHeader('Access-Control-Allow-Methods', 'GET, POST, OPTIONS, PUT, PATCH, DELETE'); // If needed
     res.setHeader('Access-Control-Allow-Headers', 'X-Requested-With,content-type'); // If needed
     res.setHeader('Access-Control-Allow-Credentials', true); // If needed
-
+    numberOfResponses -= -1; //xd
+    console.log(numberOfResponses);
     res.send(p);//responds with the link to the original .mp4 video 
-    return p; //not needed 
 };
