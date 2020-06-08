b384d02233d8145a5d08ab143d9ee48145597c2e
Rihamkh <rihamkh.114@gmail.com>
2020-06-08 18:36:15 +0300

    units change

--- src/app/payments/payments-list/payments-list.component.ts
+++ src/app/payments/payments-list/payments-list.component.ts
@@ -7,7 +7,7 @@ import { Component, OnInit } from '@angular/core';
 })
 export class PaymentsListComponent implements OnInit {
   availablePayments = false;
-  topUpApproved = false;
+  topUpApproved = true;
   constructor() {}
 
   ngOnInit() {}
