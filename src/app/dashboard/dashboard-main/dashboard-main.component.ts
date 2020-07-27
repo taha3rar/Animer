b9d064a5df2093759a138b2e26a4440d8694ad41
Rihamkh <rihamkh.114@gmail.com>
2020-07-27 13:11:48 +0300

    Changing user name bug fix

--- src/app/dashboard/dashboard-main/dashboard-main.component.ts
+++ src/app/dashboard/dashboard-main/dashboard-main.component.ts
@@ -2,6 +2,7 @@ import { Component, OnInit } from '@angular/core';
 import { ActivatedRoute } from '@angular/router';
 import { Counter } from '../models/counter';
 import { AuthenticationService } from '@app/core';
+import { User } from '@avenews/agt-sdk';
 
 @Component({
   selector: 'app-dashboard-main',
@@ -10,12 +11,14 @@ import { AuthenticationService } from '@app/core';
 })
 export class DashboardMainComponent implements OnInit {
   counter: Counter;
+  currentUser: User;
 
   constructor(private route: ActivatedRoute, private authenticationService: AuthenticationService) {}
 
   ngOnInit() {
-    this.route.data.subscribe(({ counter }) => {
+    this.route.data.subscribe(({ counter, currentUser }) => {
       this.counter = counter;
+      this.currentUser = currentUser;
     });
   }
 
