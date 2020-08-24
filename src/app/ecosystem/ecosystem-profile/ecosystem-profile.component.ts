2c7b8babb1b0deb89cf805420de67d1a805fa1dd
Rihamkh <rihamkh.114@gmail.com>
2020-08-24 12:38:47 +0300

    Ecosystem view

--- src/app/ecosystem/ecosystem-profile/ecosystem-profile.component.ts
+++ src/app/ecosystem/ecosystem-profile/ecosystem-profile.component.ts
@@ -1,15 +1,20 @@
-import { Component, OnInit } from '@angular/core';
+import { Component, OnInit, AfterViewInit } from '@angular/core';
+import * as Feather from 'feather-icons';
 
 @Component({
   selector: 'app-ecosystem-profile',
   templateUrl: './ecosystem-profile.component.html',
   styleUrls: ['./ecosystem-profile.component.scss']
 })
-export class EcosystemProfileComponent implements OnInit {
+export class EcosystemProfileComponent implements OnInit, AfterViewInit {
 
   constructor() { }
 
   ngOnInit() {
+  }
+
+  ngAfterViewInit() {
+    Feather.replace();
   }
 
 }
