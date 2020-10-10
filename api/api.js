68f5562c2264b54e3e150060f1da3e5e5c6fade3
taha3rar <taha3rar@gmail.com>
2020-10-10 14:07:39 +0300

    4anime

--- api/api.js
+++ api/api.js
@@ -1,9 +1,9 @@
-const fetch = require("node-fetch");
-const cloudscraper = require("cloudscraper");
-const cheerio = require("cheerio");
-const url = require("./urls");
-const puppeteer = require("puppeteer");
-const mongoose = require("mongoose");
+const fetch = require('node-fetch');
+const cloudscraper = require('cloudscraper');
+const cheerio = require('cheerio');
+const url = require('./urls');
+const puppeteer = require('puppeteer');
+const mongoose = require('mongoose');
 const ongoingSeries = async () => {
   const res = await fetch(`${url.BASE_URL}`);
   const body = await res.text();
@@ -11,12 +11,12 @@ const ongoingSeries = async () => {
   const promises = [];
 
   Array.from({ length: 30 }, (v, k) => {
-    $("div.main_body div.series nav.menu_series ul li")
+    $('div.main_body div.series nav.menu_series ul li')
       .eq(k + 1)
       .each((index, element) => {
         const $element = $(element);
-        const id = $element.find("a").attr("href");
-        const title = $element.find("a").text();
+        const id = $element.find('a').attr('href');
+        const title = $element.find('a').text();
         promises.push(
           animeContentHandler(id).then((extra) => ({
             title: title ? title : null,
@@ -36,17 +36,17 @@ const ongoingSeries = async () => {
 };
 
 const search = async (query) => {
-  query = query.replace(/-/g, " ");
+  query = query.replace(/-/g, ' ');
   console.log(query);
   const res = await fetch(`${url.BASE_URL}/search.html?keyword=${query}`);
   const body = await res.text();
   const $ = cheerio.load(body);
   const promises = [];
 
-  $("div.main_body div.last_episodes ul.items li").each((index, element) => {
+  $('div.main_body div.last_episodes ul.items li').each((index, element) => {
     const $element = $(element);
-    const id = $element.find("a").attr("href");
-    const title = $element.find("a").text().trim();
+    const id = $element.find('a').attr('href');
+    const title = $element.find('a').text().trim();
     promises.push(
       animeContentHandler(id).then((extra) => ({
         title: title ? title : null,
@@ -70,10 +70,10 @@ const genres = async (genre, page) => {
   const $ = cheerio.load(body);
   const promises = [];
 
-  $("div.main_body div.last_episodes ul.items li").each((index, element) => {
+  $('div.main_body div.last_episodes ul.items li').each((index, element) => {
     const $element = $(element);
-    const id = $element.find("a").attr("href");
-    const title = $element.find("a").text().trim();
+    const id = $element.find('a').attr('href');
+    const title = $element.find('a').text().trim();
     promises.push(
       animeContentHandler(id).then((extra) => ({
         title: title ? title : null,
@@ -97,11 +97,11 @@ const alphabetList = async (letter, page) => {
   const $ = cheerio.load(body);
   const promises = [];
 
-  $("div.main_body div.anime_list_body ul.listing li").each(
+  $('div.main_body div.anime_list_body ul.listing li').each(
     (index, element) => {
       const $element = $(element);
-      const id = $element.find("a").attr("href");
-      const title = $element.find("a").text().trim();
+      const id = $element.find('a').attr('href');
+      const title = $element.find('a').text().trim();
       promises.push(
         animeContentHandler(id).then((extra) => ({
           title: title ? title : null,
@@ -126,10 +126,10 @@ const newSeasons = async (page) => {
   const $ = cheerio.load(body);
   const promises = [];
 
-  $("div.main_body div.last_episodes ul.items li").each((index, element) => {
+  $('div.main_body div.last_episodes ul.items li').each((index, element) => {
     const $element = $(element);
-    const id = $element.find("div.img a").attr("href");
-    const title = $element.find("a").text().trim();
+    const id = $element.find('div.img a').attr('href');
+    const title = $element.find('a').text().trim();
     promises.push(
       animeContentHandler(id).then((extra) => ({
         title: title ? title : null,
@@ -153,10 +153,10 @@ const movies = async (page) => {
   const $ = cheerio.load(body);
   const promises = [];
 
-  $("div.main_body div.last_episodes ul.items li").each((index, element) => {
+  $('div.main_body div.last_episodes ul.items li').each((index, element) => {
     const $element = $(element);
-    const id = $element.find("div.img a").attr("href");
-    const title = $element.find("a").text().trim();
+    const id = $element.find('div.img a').attr('href');
+    const title = $element.find('a').text().trim();
     promises.push(
       animeContentHandler(id).then((extra) => ({
         title: title ? title : null,
@@ -180,10 +180,10 @@ const popular = async (page) => {
   const $ = cheerio.load(body);
   const promises = [];
 
-  $("div.main_body div.last_episodes ul.items li").each((index, element) => {
+  $('div.main_body div.last_episodes ul.items li').each((index, element) => {
     const $element = $(element);
-    const id = $element.find("div.img a").attr("href");
-    const title = $element.find("a").text().trim();
+    const id = $element.find('div.img a').attr('href');
+    const title = $element.find('a').text().trim();
     promises.push(
       animeContentHandler(id).then((extra) => ({
         title: title ? title : null,
@@ -207,11 +207,11 @@ const recentlyAddedSeries = async () => {
   const $ = cheerio.load(body);
   const promises = [];
 
-  $("div.main_body.none div.added_series_body ul.listing li").each(
+  $('div.main_body.none div.added_series_body ul.listing li').each(
     (index, element) => {
       const $element = $(element);
-      const id = $element.find("a").attr("href");
-      const title = $element.find("a").text();
+      const id = $element.find('a').attr('href');
+      const title = $element.find('a').text();
       promises.push(
         animeContentHandler(id).then((extra) => ({
           title: title ? title : null,
@@ -236,13 +236,13 @@ const recentReleaseEpisodes = async (page) => {
   const $ = cheerio.load(body);
   const promises = [];
 
-  $("div.main_body div.last_episodes.loaddub ul.items li").each(
+  $('div.main_body div.last_episodes.loaddub ul.items li').each(
     (index, element) => {
       const $element = $(element);
-      const id = $element.find("p.name a").attr("href");
-      const title = $element.find("p.name a").text();
+      const id = $element.find('p.name a').attr('href');
+      const title = $element.find('p.name a').text();
       const episode = parseInt(
-        $element.find("p.episode").text().match(/\d+/g),
+        $element.find('p.episode').text().match(/\d+/g),
         10
       );
       promises.push(
@@ -271,28 +271,28 @@ const animeEpisodeHandler = async (id) => {
   const $ = cheerio.load(body);
   const promises = [];
 
-  $("div#wrapper_bg").each((index, element) => {
+  $('div#wrapper_bg').each((index, element) => {
     const $element = $(element);
     const animeId = $element
-      .find("div.anime_video_body div.anime_video_body_cate div.anime-info a")
-      .attr("href");
+      .find('div.anime_video_body div.anime_video_body_cate div.anime-info a')
+      .attr('href');
     const category = $element
-      .find("div.anime_video_body div.anime_video_body_cate a")
-      .attr("href")
-      .split("/")[2]
+      .find('div.anime_video_body div.anime_video_body_cate a')
+      .attr('href')
+      .split('/')[2]
       .trim();
 
     const servers = [];
-    $element.find("div.anime_muti_link ul li").each((j, el) => {
+    $element.find('div.anime_muti_link ul li').each((j, el) => {
       const $el = $(el);
       const name = $el
-        .find("a")
+        .find('a')
         .text()
-        .substring(0, $el.find("a").text().lastIndexOf("C"))
+        .substring(0, $el.find('a').text().lastIndexOf('C'))
         .trim();
-      let iframe = $el.find("a").attr("data-video");
-      if (iframe.startsWith("//")) {
-        iframe = $el.find("a").attr("data-video").slice(2);
+      let iframe = $el.find('a').attr('data-video');
+      if (iframe.startsWith('//')) {
+        iframe = $el.find('a').attr('data-video').slice(2);
       }
       servers.push({
         name: name,
@@ -322,61 +322,61 @@ const animeContentHandler = async (id) => {
   const $ = cheerio.load(body);
   const promises = [];
 
-  $("div#wrapper_bg").each((index, element) => {
+  $('div#wrapper_bg').each((index, element) => {
     const $element = $(element);
-    const img = $element.find("div.anime_info_body_bg img").attr("src");
+    const img = $element.find('div.anime_info_body_bg img').attr('src');
     const synopsis = $element
-      .find("div.anime_info_body_bg p.type")
+      .find('div.anime_info_body_bg p.type')
       .eq(1)
       .text();
     const genres = [];
     $element
-      .find("div.anime_info_body_bg p.type")
+      .find('div.anime_info_body_bg p.type')
       .eq(2)
-      .find("a")
+      .find('a')
       .each((j, el) => {
         const $el = $(el);
-        const genre = $el.attr("href").split("/")[4];
+        const genre = $el.attr('href').split('/')[4];
         genres.push(genre);
       });
     const released = parseInt(
-      $element.find("div.anime_info_body_bg p.type").eq(3).text().match(/\d+/g),
+      $element.find('div.anime_info_body_bg p.type').eq(3).text().match(/\d+/g),
       10
     );
     const status = $element
-      .find("div.anime_info_body_bg p.type")
+      .find('div.anime_info_body_bg p.type')
       .eq(4)
       .text()
-      .replace("Status:", "")
+      .replace('Status:', '')
       .trim();
     const otherName = $element
-      .find("div.anime_info_body_bg p.type")
+      .find('div.anime_info_body_bg p.type')
       .eq(5)
       .text()
-      .replace("Other name:", "")
+      .replace('Other name:', '')
       .trim();
-    const liTotal = $("div.anime_video_body ul#episode_page li").length;
+    const liTotal = $('div.anime_video_body ul#episode_page li').length;
     let totalEpisodes = 0;
     if (
-      $("div.anime_video_body ul#episode_page li")
+      $('div.anime_video_body ul#episode_page li')
         .eq(liTotal - 1)
-        .find("a")
+        .find('a')
         .text()
-        .includes("-")
+        .includes('-')
     ) {
       totalEpisodes = parseInt(
-        $("div.anime_video_body ul#episode_page li")
+        $('div.anime_video_body ul#episode_page li')
           .eq(liTotal - 1)
-          .find("a")
+          .find('a')
           .text()
-          .split("-")[1],
+          .split('-')[1],
         10
       );
     } else {
       totalEpisodes = parseInt(
-        $("div.anime_video_body ul#episode_page li")
+        $('div.anime_video_body ul#episode_page li')
           .eq(liTotal - 1)
-          .find("a")
+          .find('a')
           .text(),
         10
       );
@@ -412,11 +412,11 @@ const decodeVidstreamingIframeURL = async (url) => {
     .match(match)
     .filter((url) => {
       return (
-        (!url.includes(".mp4upload") && url.includes(".mp4")) ||
-        url.includes("m3u8") ||
-        url.includes("vidstreaming.io/goto.php") ||
-        (url.includes("gogo-stream.com") && url.includes("url")) ||
-        url.includes("storage.googleapis.com")
+        (!url.includes('.mp4upload') && url.includes('.mp4')) ||
+        url.includes('m3u8') ||
+        url.includes('vidstreaming.io/goto.php') ||
+        (url.includes('gogo-stream.com') && url.includes('url')) ||
+        url.includes('storage.googleapis.com')
       );
     });
   console.log(_URLs);
@@ -424,7 +424,7 @@ const decodeVidstreamingIframeURL = async (url) => {
   Array.from({ length: _URLs.length }, (v, k) => {
     const option = k + 1;
     let url = _URLs[k];
-    if (!url.includes("https://")) {
+    if (!url.includes('https://')) {
       url = `https://${url}`;
     }
     URLs.push(url);
@@ -436,11 +436,11 @@ const anime = async (url) => {
   const _url = url;
   url = _url;
   let promises = [];
-  let splitted = _url.split("-episode-");
+  let splitted = _url.split('-episode-');
   const name = splitted[0];
   const num = parseInt(splitted[1], 10);
   console.log(_url); //anime name and episode in this case
-  if (_url.includes("-episode")) {
+  if (_url.includes('-episode')) {
     const ep = await animeEpisodeHandler(_url);
     if (
       ep &&
@@ -450,9 +450,9 @@ const anime = async (url) => {
       ep[0].servers[0].iframe
     ) {
       let link;
-      if (!ep[0].servers[0].iframe.includes("http")) {
-        console.log("xd");
-        link = "https://" + ep[0].servers[0].iframe;
+      if (!ep[0].servers[0].iframe.includes('http')) {
+        console.log('xd');
+        link = 'https://' + ep[0].servers[0].iframe;
       } else {
         link = ep[0].servers[0].iframe;
       }
@@ -460,13 +460,13 @@ const anime = async (url) => {
       let vid = await decodeVidstreamingIframeURL(link);
       if (vid.length === 0) {
         vid = await decodeVidstreamingIframeURL(
-          link.replace("streaming.php", "loadserver.php")
+          link.replace('streaming.php', 'loadserver.php')
         );
         promises = vid;
         if (ep[0].servers && vid.length === 0) {
-          console.log("here");
+          console.log('here');
           let alt = ep[0].servers.find((e, i) => {
-            if (e.name.toLowerCase().includes("mp4upload")) return e;
+            if (e.name.toLowerCase().includes('mp4upload')) return e;
           });
           if (alt) {
             promises = await pup(alt.iframe);
@@ -477,17 +477,18 @@ const anime = async (url) => {
       }
       console.log(splitted);
       let vi = await decodeVidstreamingIframeURL(
-        "https://4anime.to/" + splitted[0] + "-episode-0" + splitted[1]
+        'https://4anime.to/' + splitted[0] + '-episode-0' + splitted[1]
       );
-      console.log(vi, "s");
       if (vi.length === 0) {
         vi = await decodeVidstreamingIframeURL(
-          "https://4anime.to/" + splitted[0] + "-episode-" + splitted[1]
+          'https://4anime.to/' + splitted[0] + '-episode-' + splitted[1]
         );
       }
       if (vi.length !== 0) {
         promises.push(vi[0]);
       }
+      console.log(vi);
+      console.log('that was vi');
       const AU = await getEp(name, num);
       if (AU) {
         promises.push(AU);
@@ -499,7 +500,7 @@ const anime = async (url) => {
       console.log(promises);
       return Promise.all(promises);
     }
-    return Promise.all("sad");
+    return Promise.all('sad');
   }
 };
 const pup = async (alt) => {
@@ -507,16 +508,16 @@ const pup = async (alt) => {
     const ure = [];
     const browser = await puppeteer.launch({
       args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox", // these two args for heroku to use puppeteer
+        '--no-sandbox',
+        '--disable-setuid-sandbox', // these two args for heroku to use puppeteer
       ],
     });
     const page = await browser.newPage();
-    await page.goto(alt, { waitUntil: "domcontentloaded" });
+    await page.goto(alt, { waitUntil: 'domcontentloaded' });
     console.log(alt);
-    page.on("response", async (resp) => {
+    page.on('response', async (resp) => {
       uri = resp.url();
-      if (uri && uri.includes("video.mp4") && uri.includes("mp4upload")) {
+      if (uri && uri.includes('video.mp4') && uri.includes('mp4upload')) {
         ure.push(resp.url());
         console.log(resp.url());
         resolve(ure);
@@ -530,24 +531,24 @@ const pupe = async (alt) => {
   const re = await fetch(alt);
   const body = await re.text();
   const $ = cheerio.load(body);
-  let match = "Solidfiles";
-  $("ul.TPlayerNv li").each((index, element) => {
+  let match = 'Solidfiles';
+  $('ul.TPlayerNv li').each((index, element) => {
     const $element = $(element);
-    if ($element.find("span").text().includes(match)) {
+    if ($element.find('span').text().includes(match)) {
       i = index + 1;
-      match = "oiasjdiowqdjijwqdiowq";
+      match = 'oiasjdiowqdjijwqdiowq';
       // console.log(i);
     }
   });
   if (i !== -1) {
     let frame = await puppet(alt, i);
-    if (frame !== "a") {
+    if (frame !== 'a') {
       let result = await matcher(frame);
       // console.log(result[0]);
-      return Promise.resolve("http://" + result[0]);
-    } else Promise.resolve("a");
+      return Promise.resolve('http://' + result[0]);
+    } else Promise.resolve('a');
   } else {
-    return Promise.resolve("a");
+    return Promise.resolve('a');
   }
 };
 const matcher = async (frame) => {
@@ -557,7 +558,7 @@ const matcher = async (frame) => {
   const _URLs = String(data)
     .match(match)
     .filter((url) => {
-      return url.includes(".mp4");
+      return url.includes('.mp4');
     });
   return Promise.resolve(_URLs);
 };
@@ -567,23 +568,23 @@ const puppet = async (alt, i) => {
     const ure = [];
     const browser = await puppeteer.launch({
       args: [
-        "--no-sandbox",
-        "--disable-setuid-sandbox", // these two args for heroku to use puppeteer
+        '--no-sandbox',
+        '--disable-setuid-sandbox', // these two args for heroku to use puppeteer
       ],
       headless: true,
     });
     try {
       const page = await browser.newPage();
-      await page.goto(alt, { waitUntil: "domcontentloaded" });
+      await page.goto(alt, { waitUntil: 'domcontentloaded' });
       try {
         await page.click(`ul.TPlayerNv li[data-tplayernv='Opt${i}']`);
       } catch (e) {
-        resolve("a");
+        resolve('a');
         console.log(e);
       }
-      page.on("response", async (resp) => {
+      page.on('response', async (resp) => {
         uri = resp.url();
-        if (uri && uri.includes("solidfiles")) {
+        if (uri && uri.includes('solidfiles')) {
           ure.push(resp.url());
           // console.log(resp.url());
           hi = true;
@@ -592,7 +593,7 @@ const puppet = async (alt, i) => {
         }
         setTimeout(() => {
           if (!hi) {
-            resolve("a");
+            resolve('a');
           }
 
           return;
@@ -606,7 +607,7 @@ const puppet = async (alt, i) => {
 };
 const getEp = async (name, num) => {
   try {
-    const eps = await mongoose.model("Animes").findOne({ name: name }).exec();
+    const eps = await mongoose.model('Animes').findOne({ name: name }).exec();
     if (eps == null) {
       return Promise.resolve(false);
     } else {
@@ -624,15 +625,15 @@ const getEp = async (name, num) => {
 const getAr = async (name, num) => {
   try {
     const eps = await mongoose
-      .model("Animes")
-      .findOne({ name: name + "-ar" })
+      .model('Animes')
+      .findOne({ name: name + '-ar' })
       .exec();
     if (eps == null) {
-      console.log("not found");
+      console.log('not found');
       return Promise.resolve(false);
     } else {
       if (num > eps.episodes.length) {
-        console.log("done");
+        console.log('done');
         return Promise.resolve(false);
       } else if (num > 0) {
         console.log(eps.episodes[num - 1]);
@@ -659,9 +660,9 @@ const ar = async (link) => {
       .match(match)
       .filter((url) => {
         a = true;
-        return url.includes(".mp4");
+        return url.includes('.mp4');
       });
-    resolve("http://" + _URLs[0]);
+    resolve('http://' + _URLs[0]);
   });
 };
 module.exports = {
